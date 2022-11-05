84bc12e4fdfa8cb9297bbc02f2620db9cff73282
Lucien Huang <superstarhyc@qq.com>
2022-11-05 10:29:33 +0800

    Update auction.ts

--- src/data/auction.ts
+++ src/data/auction.ts
@@ -84,7 +84,7 @@ export const auction: Auction = {
   },
   actions: {
     text: 'Join the Auction Now',
-    button: 'BID ON OPENSEA', // Bid on Opensea
+    button: 'VIEW ON OPENSEA', // Bid on Opensea
     link: 'https://opensea.io/collection/e4crangersultimate',
     buttonStyle: {
       backgroundColor: '#2081E2' // #2081E2
