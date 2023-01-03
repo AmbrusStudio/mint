c2339ceed845550195e21009b579c2dcea7c5607
Garfield550 <Garfield550@users.noreply.github.com>
2023-01-03 09:21:56 +0000

    feat: add free mint api

--- src/api/mint/index.ts
+++ src/api/mint/index.ts
@@ -16,19 +16,41 @@ export async function getFlashMintInfo(): Promise<FlashMint> {
   return Promise.resolve(flashMint)
 }
 
-const apiPath = getViteEnv('VITE_MINT_ACCESS_BACKEND_API_PATH')
+const mintApiPath = getViteEnv('VITE_MINT_ACCESS_BACKEND_API_PATH')
+const hiveApiPath = getViteEnv('VITE_NFT_HIVE_BACKEND_API_PATH')
 
-type GetSignature = {
-  data: string[]
-}
-export async function getSignature(
+type GetMintSignature = { data: string[] }
+export async function getMintSignature(
   address: string,
   saleKind: MintSaleKind,
   edition: MintEditionValue
 ): Promise<string[]> {
-  console.debug('getSignature', address, saleKind, edition)
-  const { data: proof } = await mintRequest.get<GetSignature>(`${apiPath}/hasRole`, {
+  console.debug('getMintSignature', address, saleKind, edition)
+  const { data: proof } = await mintRequest.get<GetMintSignature>(`${mintApiPath}/hasRole`, {
     params: { address, saleKind, edition }
   })
   return proof.data
+}
+
+type GetHiveSaleStatus = { soldCount: number }
+export async function getHiveSaleStatus(): Promise<GetHiveSaleStatus> {
+  const { data } = await mintRequest.get<GetHiveSaleStatus>(`${hiveApiPath}/saleStatus`)
+  return data
+}
+
+type GetMintSignatureCode = { code: string }
+export async function getMintHiveSignatureCode(address: string): Promise<GetMintSignatureCode> {
+  const { data } = await mintRequest.post<GetMintSignatureCode>(`${hiveApiPath}/mint-prepare`, {
+    address
+  })
+  return data
+}
+
+type MintHiveNft = { tokenId: number; transferId: number }
+export async function mintHiveNft(address: string, signature: string): Promise<MintHiveNft> {
+  const { data } = await mintRequest.post<MintHiveNft>(`${hiveApiPath}/mint`, {
+    address,
+    signature
+  })
+  return data
 }
