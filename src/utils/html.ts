6890e5843f959e0ac13539ec1527533d06a4fcae
Garfield550 <Garfield550@users.noreply.github.com>
2023-01-04 07:26:59 +0000

    fix: typo and make anchor target is new tab

--- src/utils/html.ts
+++ src/utils/html.ts
@@ -3,6 +3,12 @@ import createDOMPurify from 'dompurify'
 export function cleanupHTML(dirty: string): string {
   if (typeof window === 'undefined') return dirty
   const DOMPurify = createDOMPurify(window)
+  DOMPurify.addHook('afterSanitizeAttributes', function (node) {
+    if (node.tagName === 'A' && 'target' in node) {
+      node.setAttribute('target', '_blank')
+      node.setAttribute('rel', 'noopener')
+    }
+  })
   const clean = DOMPurify.sanitize(dirty)
   return clean
 }
